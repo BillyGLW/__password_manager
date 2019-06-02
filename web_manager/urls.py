2f6e76028603b31d156c9a7e5bbef51790a1b7f8
BillyGLW <malllock212@gmail.com>
2019-06-02 14:44:03 +0200

    task2

--- web_manager/urls.py
+++ web_manager/urls.py
@@ -25,6 +25,13 @@ urlpatterns = [
     path('', index, name="index"),
     path('login/', login_user, name="login_user"),
     path('logout/', wm_logout, name="wm_logout"),
-    url(r'^profile/(?P<username>[\w.@+-]+)/update/', wm_update, name="wm_update"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/delete/', wm_delete, name="wm_delete"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/update/$', wm_update, name="wm_update"),
     url(r'^profile/(?P<username>[\w.@+-]+)/$', manage_password, name="manage_password"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/save/$', wm_password_update, name="wm_password_update"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/share/$', shared_link, name="shared_link"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/get_hash/(?P<hash>[\w.@+-]+)/(?P<enc>[-\w/\\=+/\s]+)/$', shared_link_update, name="shared_link_update"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/get_hash/$', wm_shared_link_generate, name="wm_shared_link_generate"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/get_hash/is_valid$', wm_shared_password_handle, name="wm_shared_password_handle"),
+
 ]
